63fee4bdf23a23a8498eb196484104411729a1ae
H3x0R <opengamertips@gmail.com>
2021-03-26 10:08:33 -0500

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,38 +1,48 @@
-getgenv().IpProtection = true
-spoofedIP = "0.0.0.0"
+local spoofedIP = "never gonna give you up never gonna let you down never gonna run around and desert you never gonna make you cry never gonna say goodbye never gonna tell a lie and hurt you - H3x0R" -- The IP you want the logger to THINK it is.
+-- Removed vulnerabillity to disable the hooks.
 
-
-IP_Trackers = {
-    "https://v4.ident.me",
-    "https://api.ipify.org",
-    "https://httpbin.org/get"
+local IP_Trackers = {
+    ["v4.ident.me"] = true,
+    ["v4.ident.me/"] = true, -- Trailing slash check
+    
+    ["api.ipify.org"] = true,
+    ["api.ipify.org/"] = true,
+    
+    ["httpbin.org/get"] = true,
+    ["httpbin.org/get/"] = true,
+    
+    ["icanhazip.com"] = true,
+    ["icanhazip.com/"] = true
 }
 
-local old
-old = hookfunction(game.HttpGet, function(self, url)
-        -- Checking if the protection is enabled
-    if type(url) == "string" and getgenv().IpProtection then
-        -- Checks if the URL is in the IP_Trackers table
-        if table.find(IP_Trackers, url) then
-            warn(url,"tried to log your IP. It was protected.")
+local function IsTracker(Url)
+    if IP_Trackers[string.gsub(Url, "https://", "")] or IP_Trackers[string.gsub(Url, "http://", "")] then -- Remove http/s to check domain.
+        return true -- Tracker found!
+    end
+    return false
+end
+
+local old;
+old = hookfunction(game.HttpGet, function(self, url, ...)
+    if type(url) == "string" then
+        if IsTracker(url) then
+            warn(url.." was used to attempt to log your IP. This request was intercepted.")
             return spoofedIP -- Spoofed IP
         end
     end
-    return old(self, url)
+    return old(self, url, ...)
 end)
 
-local oldSyn
-oldSyn = hookfunction(syn.request,function(a,b)
-    if type(a) == "table" and getgenv().IpProtection then
-        for i,v in pairs(a) do
-            if i == "Url" and table.find(IP_Trackers, v) then
-                warn(v,"tried to log your IP. It was protected.")
-                return {
-                    StatusCode = 200,
-                    Body = spoofedIP
-                }
-            end
+local oldSyn;
+oldSyn = hookfunction(syn.request, function(a, b, ...)
+    if type(a) == "table" then
+        if IsTracker(a.Url) then
+            warn(a.Url.." was used to attempt to log your IP. This request was intercepted.")
+            return {
+                StatusCode = 200,
+                Body = spoofedIP
+            }
         end
     end
-    return oldSyn(a,b)
+    return oldSyn(a, b, ...)
 end)
